19a2df0da6f6a764de95578056f7ff7bc686e77e
jszt <jean.szturemski@gmail.com>
2020-12-13 09:21:26 +0000

    init

---
+++ serveur.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const WebSocket = require('ws')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const server = new WebSocket.Server({
+  port: 8080
+});
+
+let sockets = [];
+server.on('connection', function(socket) {
+  sockets.push(socket);
+
+  // When you receive a message, send that message to every socket.
+  socket.on('message', function(msg) {
+    ///sockets.forEach(s => res.send(msg));
+	console.log(msg);
+  });
+
+  // When a socket closes, or disconnects, remove it from the array.
+  socket.on('close', function() {
+    sockets = sockets.filter(s => s !== socket);
+  });
+});
